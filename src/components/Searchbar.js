9162c4a0609a3619f436cfc6e97cf7a7d32b2610
sarahrosecooper <sarahrosecooperx@gmail.com>
2021-03-08 02:28:30 -0500

    feat: (Searchbar.js)

--- src/components/Searchbar.js
+++ src/components/Searchbar.js
@@ -1,7 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
+
+// guideline: https://medium.com/@carlie.anglemire/search-bar-in-react-with-a-3rd-party-api-ced92c940236
 
 const Searchbar = () => {
-  return <div>this is the searchbar component</div>;
+  const [search, setSearch] = useState({
+    searchTerm: "",
+  });
+
+  // console.log(search);
+
+  const handleChange = (e) => {
+    setSearch({
+      ...search,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const submitSearch = (e) => {
+    e.preventDefault();
+    setSearch({
+      searchTerm: "",
+    });
+  };
+
+  return (
+    <div>
+      this is the searchbar component
+      <form onSubmit={submitSearch}>
+        <label htmlFor="searchTerm" />
+        <input
+          id="searchTerm"
+          type="text"
+          name="searchTerm"
+          value={search.searchTerm}
+          onChange={handleChange}
+          placeholder="search here"
+        />
+      </form>
+    </div>
+  );
 };
 
 export default Searchbar;

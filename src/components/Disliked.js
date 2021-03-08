6d332174e75876d3a6694e1d488071afe212cb59
sarahrosecooper <sarahrosecooperx@gmail.com>
2021-03-07 22:39:51 -0500

    feat: (All) setup basic file structure for the time being, components, etc

---
+++ src/components/Disliked.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+// This will render shows you haven't finished because you didn't like them or that you didn't like
+
+const Disliked = () => {
+  return <div>this is the disliked component</div>;
+};
+
+export default Disliked;

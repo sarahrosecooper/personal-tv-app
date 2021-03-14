6eb645630699608623bbb158bf90211f3b176f1f
sarahrosecooper <sarahrosecooperx@gmail.com>
2021-03-14 00:27:53 -0500

    (feat) AllResults.js: data rendering succesfully to component via browser, working on efficient removal of HTML tags, routing on title Click working

--- src/components/AllResults.js
+++ src/components/AllResults.js
@@ -1,18 +1,41 @@
 import React from "react";
 import { connect } from "react-redux";
+import { useHistory } from "react-router-dom";
 
 const AllResults = (props) => {
   console.log("these are the AllResults props", props);
   console.log(
-    "these are the AllResults props getting deeper",
-    props.tv_show_results[0].show.id
+    "these are the AllResults props getting deeper"
+    // props.tv_show_results[0].show.id
   );
 
+  const { push } = useHistory();
+
+  const handleClick = () => {
+    push("/topfive");
+  };
+  // const image = props.tv_show_results.show.image.medium ===
   return (
     <div>
       this is the all results component<br></br>
       {props.tv_show_results.map((item) => {
-        return <div>{item.show.name}</div>;
+        return (
+          <div key={item.show.id}>
+            <div onClick={handleClick} className="name">
+              {item.show.name}
+            </div>
+            <br></br>
+            <img
+              src={item.show.image === null ? null : item.show.image.medium}
+            />
+            <br></br>
+            {item.show.summary
+              .replace(`<p>`, "")
+              .replace(`<b>`, "")
+              .replace(`</p>`, "")
+              .replace(`</b>`, "")}
+          </div>
+        );
       })}
     </div>
   );

5ffcd466e4783b652ff8c3d11baded264c3911d0
sarahrosecooper <sarahrosecooperx@gmail.com>
2021-04-19 18:47:47 -0400

    Cleaning up

--- src/components/AllResults.js
+++ src/components/AllResults.js
@@ -1,35 +1,20 @@
 import React, { useState } from "react";
 import { connect } from "react-redux";
-import { useParams, useHistory } from "react-router-dom";
 import SingleShow from "./SingleShow.js";
 
 const AllResults = (props) => {
   const [selectedTitle, setSelectedTitle] = useState();
   const [displayShow, setDisplayShow] = useState(false);
 
-  console.log("these are the AllResults props", props);
-  console.log(
-    "these are the AllResults props getting deeper"
-    // props.tv_show_results[0].show.id
-  );
-
-  const { push } = useHistory();
-  const { id } = useParams();
-
   const handleClick = (item) => {
-    console.log("this is the item in AllResults", item.show);
-    // push(`/singleshow/${item.show.id}`);
     setSelectedTitle(item.show);
     displayTheShow();
-    console.log("this is the selected title", selectedTitle);
   };
 
   const displayTheShow = () => {
     setDisplayShow(!displayShow);
-    console.log(displayShow);
   };
 
-  // const image = props.tv_show_results.show.image.medium ===
   return (
     <div>
       {displayShow ? (
@@ -38,7 +23,6 @@ const AllResults = (props) => {
         props.tv_show_results.map((item) => {
           return (
             <div key={item.show.id}>
-              {/* <div onClick={handleClick} className="name"> */}
               <div
                 onClick={() => {
                   handleClick(item);
@@ -61,32 +45,6 @@ const AllResults = (props) => {
           );
         })
       )}
-
-      {/* {props.tv_show_results.map((item) => {
-        return (
-          <div key={item.show.id}> */}
-      {/* <div onClick={handleClick} className="name"> */}
-      {/* <div
-              onClick={() => {
-                handleClick(item);
-              }}
-            >
-              {item.show.name}
-
-              <br></br>
-              <img
-                src={item.show.image === null ? null : item.show.image.medium}
-              />
-            </div>
-            <br></br>
-            {item.show.summary
-              .replace(`<p>`, "")
-              .replace(`<b>`, "")
-              .replace(`</p>`, "")
-              .replace(`</b>`, "")}
-          </div>
-        );
-      })} */}
     </div>
   );
 };
--- src/components/SingleShow.js
+++ src/components/SingleShow.js
@@ -1,40 +1,22 @@
 import React from "react";
-import { connect } from "react-redux";
 
-const SingleShow = (props) => {
-  console.log("this is the single show result", props);
-  console.log(
-    "these are the AllResults props getting deeper"
-    // props.tv_show_results[0].show.id
-  );
+const SingleShow = ({ show }) => {
+  // FIXME why doesn't the searchBar work when only a single result is showing, is it the state in the reducer?
+  console.log("this is the single show result", show);
+
   return (
     <div>
-      This is the single show component <br></br>
-      {/* {props.tv_show_results.map((item) => {
-        return (
-          <div key={item.show.id}>
-            <div className="name">{item.show.name}</div>
-            <br></br>
-            <img
-              src={item.show.image === null ? null : item.show.image.medium}
-            />
-            <br></br>
-            {item.show.summary
-              .replace(`<p>`, "")
-              .replace(`<b>`, "")
-              .replace(`</p>`, "")
-              .replace(`</b>`, "")}
-          </div>
-        );
-      })} */}
+      {show.name}
+      <br></br>
+      <img src={show.image === null ? null : show.image.medium} />
+      <br></br>
+      {show.summary
+        .replace(`<p>`, "")
+        .replace(`<b>`, "")
+        .replace(`</p>`, "")
+        .replace(`</b>`, "")}
     </div>
   );
 };
 
-const mapStateToProps = (state) => {
-  return {
-    tv_show_results: state.SEARCHREDUCER.tv_show_results,
-  };
-};
-
-export default connect(mapStateToProps, {})(SingleShow);
+export default SingleShow;
